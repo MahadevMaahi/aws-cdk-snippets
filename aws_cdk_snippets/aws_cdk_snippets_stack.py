07361675936239fdf45581db9e342023863b5f19
Sai Mahadev <mahadevvone@gmail.com>
2022-12-27 14:13:47 +0530

    Base CDK Snippet

---
+++ aws_cdk_snippets/aws_cdk_snippets_stack.py
@@ -0,0 +1,23 @@
+from aws_cdk import (
+    # Duration,
+    aws_s3 as _s3,
+    core
+    # aws_sqs as sqs,
+)
+from constructs import Construct
+
+class AwsCdkSnippetsStack(core.Stack):
+
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+        super().__init__(scope, construct_id, **kwargs)
+
+        # The code that defines your stack goes here
+        _s3.Bucket(
+            self,
+            "myBucketId"
+        )
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "AwsCdkSnippetsQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
